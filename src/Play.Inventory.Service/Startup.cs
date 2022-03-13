85f24ab4abba8971fc38aebd72b139e567fd28a9
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-03-13 11:31:12 -0300

    feat: Add a random miliseconds retry attempts.
    To not force server same time from many diferent requests

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -31,13 +31,15 @@ namespace Play.Inventory.Service
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");
             })
             .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
                 5,
-                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
                 onRetry: (outcome, timeSpan, retryAttempt) =>
                 {
                     // DO NOT USE IN PRODUCTION: THIS CAUSE ANOTHER INSTANCE OF SERVICEPROVIDER
