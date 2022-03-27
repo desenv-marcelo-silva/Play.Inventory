75c9638963aca1a87386653c26235432a1191f32
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-03-27 11:53:37 -0300

    feat: Add Cors to use frontend

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -18,6 +18,8 @@ namespace Play.Inventory.Service
 {
     public class Startup
     {
+        private const string AllowOriginSetting = "AllowedOrigin";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -51,6 +53,13 @@ namespace Play.Inventory.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
+
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowOriginSetting])
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
             }
 
             app.UseHttpsRedirection();
